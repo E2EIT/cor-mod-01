e139b251227b1726553cd0f8e255590cfe965303
Robert <E2EIT@users.noreply.github.com>
2015-06-11 19:15:43 -0700

    v0.1: Initial base app.

---
+++ www/js/index.js
@@ -0,0 +1,45 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        app.run();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+window.onload = function run() {
+    console.log("App running...");
+    var addBtnElm = document.getElementById("addBtn");
+    addBtnElm.addEventListener('click',
+        function addItem() {
+            console.log("Add button clicked.");
+        }, false);
+    console.log('Click event listener added.');
+
+};
